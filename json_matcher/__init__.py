7a1c5a634e50b82fa91fec8eb4a653ea6d00efab
임호성 <greg.yim@kakaocorp.com>
2019-10-08 18:52:32 +0900

    fix version

--- json_matcher/__init__.py
+++ json_matcher/__init__.py
@@ -3,6 +3,4 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = '0.0.1'
-
 from .json_matcher import *
--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ import os
 import setuptools
 
 module_path = os.path.join(os.path.dirname(__file__), 'json_matcher/__init__.py')
-version_line = [line for line in open(module_path)
-                if line.startswith('__version__')][0]
-
-__version__ = version_line.split('__version__ = ')[-1][1:][:-2]
 
 # scripts
 scripts = [ 'bin/json_match', 'bin/jrep' ]
@@ -17,8 +13,8 @@ packages = setuptools.find_packages(exclude=['tests'])
 
 setuptools.setup(
     name="json_matcher",
-    version=__version__,
-    url="https://github.daumkakao.com/greg.yim/json_matcher",
+    version="0.0.1",
+    url="https://github.com/hosung-yim/json_matcher",
 
     author="Greg.YIM",
     author_email="greg.yim@kakaocorp.com",
