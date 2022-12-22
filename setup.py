1dec1a29eb51f613b02a8f210d8dfe9ef9395f15
Hosung Yim <hosung.yim@gmail.com>
2022-12-22 15:16:35 +0900

    0.0.9

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ packages = setuptools.find_packages(exclude=['tests'])
 
 setuptools.setup(
     name="json_matcher",
-    version="0.0.8",
+    version="0.0.9",
     url="https://github.com/hosung-yim/json_matcher",
-    dowload_url="https://github.com/hosung-yim/json_matcher/archive/refs/tags/0.0.8.tar.gz",
+    dowload_url="https://github.com/hosung-yim/json_matcher/archive/refs/tags/0.0.9.tar.gz",
 
     author="Greg.YIM",
     author_email="greg.yim@kakaocorp.com",
